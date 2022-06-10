fc1903106a1eb1a415fe57d2097463d4662abec4
nikothegamer <95710125+nikothegamer100@users.noreply.github.com>
2022-06-10 14:59:02 +0300

    Update MainModule.lua
    Update 0.2

--- MainModule.lua
+++ MainModule.lua
@@ -1,17 +1,26 @@
+local ds = game:GetService("DataStoreService"):GetDataStore("Chat datastore")
+
 return function(data)
 	local success, err = pcall(function()
 		for i,v in pairs(data.moderators) do
-			script.chatServer.setMod:Fire(v)
+			local data = ds:GetAsync(v)
+			if data == nil then
+				ds:SetAsync(v, {true})
+			end
 		end
-		script.chatServer.setSettings:Fire("message_deletion",data.message_deletion)
+		if data.colors then
+			if data.colors.messageInput then
+				script.customChat.mainFrame.MessageBox.Text.TextColor3 = data.colors.messageInput.text
+			end
+		end
+		--script.chatServer.setSettings:FireServer("message_deletion",data.message_deletion)
 		script.chat.Parent = game.ReplicatedStorage
 		script.customChat.Parent = game.StarterGui
 		script.chatServer.Parent = game.ServerScriptService
-		wait()
+		task.wait()
 		script:Destroy()
 	end)
 	if not success then
-		warn("Failure while loading the chat.")
-		warn(err)
+		warn("Failure while loading the chat; error: "..err)
 	end
 end
